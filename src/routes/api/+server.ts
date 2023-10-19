a5293954262c42b4647c62506e4746d18a2d8e43
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-19 14:37:04 +0700

    feat : add API template

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,5 +1,47 @@
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-	return new Response();
+export const GET: RequestHandler = async ({ url }) => {
+	const params = url.searchParams;
+	const id = params.get("id") as string
+
+	try {
+
+		return new Response();
+	} catch (error: unknown) {
+		if (error instanceof Error) return new Response(error.message, { status: 400 });
+		return new Response(JSON.stringify(error), { status: 400 });
+	}
 };
+
+export const POST: RequestHandler = async ({ request }) => {
+	const { } = (await request.json()) as {}
+	try {
+
+		return new Response();
+	} catch (error: unknown) {
+		if (error instanceof Error) return new Response(error.message, { status: 400 });
+		return new Response(JSON.stringify(error), { status: 400 });
+	}
+}
+
+export const PATCH: RequestHandler = async ({ request }) => {
+	const { } = (await request.json()) as {}
+	try {
+
+		return new Response();
+	} catch (error: unknown) {
+		if (error instanceof Error) return new Response(error.message, { status: 400 });
+		return new Response(JSON.stringify(error), { status: 400 });
+	}
+}
+
+export const DELETE: RequestHandler = async ({ request }) => {
+	const { } = (await request.json()) as {}
+	try {
+
+		return new Response();
+	} catch (error: unknown) {
+		if (error instanceof Error) return new Response(error.message, { status: 400 });
+		return new Response(JSON.stringify(error), { status: 400 });
+	}
+}
