ead188488725852e9c4ec13e180b86ce84faefe6
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-19 16:36:07 +0700

    fix : change from return text to json

--- src/routes/api/auth/signin/+server.ts
+++ src/routes/api/auth/signin/+server.ts
@@ -5,7 +5,7 @@ export const POST: RequestHandler = async ({ request }) => {
 	const body = (await request.json()) as SignInProps;
 
 	try {
-		return Response.json(await signIn(body))
+		return Response.json(await signIn(body));
 	} catch (error: unknown) {
 		if (error instanceof Error) return Response.json(error.message, { status: 400 });
 		return Response.json(error, { status: 400 });
--- src/routes/api/auth/users/+server.ts
+++ src/routes/api/auth/users/+server.ts
@@ -23,7 +23,7 @@ export const DELETE: RequestHandler = async ({ request }) => {
 	const body = (await request.json()) as DeleteUserProps;
 	if (!body.id || body.id === undefined) return Response.json('No id provided.', { status: 400 });
 	try {
-		return Response.json(await deleteUser(body))
+		return Response.json(await deleteUser(body));
 	} catch (error: unknown) {
 		if (error instanceof Error) return Response.json(error.message, { status: 400 });
 		return Response.json(error, { status: 400 });
@@ -32,7 +32,8 @@ export const DELETE: RequestHandler = async ({ request }) => {
 
 export const PATCH: RequestHandler = async ({ request }) => {
 	const body = (await request.json()) as UpdateUserProps;
-	if (!body.name || body.name === undefined) return new Response('No name provided.', { status: 400 });
+	if (!body.name || body.name === undefined)
+		return new Response('No name provided.', { status: 400 });
 	try {
 		return Response.json(await updateUser(body));
 	} catch (error: unknown) {
