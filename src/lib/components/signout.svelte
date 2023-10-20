32e12d8579ff86076710339549a6934b4a1cfba3
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-20 13:32:24 +0700

    feat : signout component

---
+++ src/lib/components/signout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import axios from 'axios';
+
+	const onClick = async () => {
+		const { status, statusText } = await axios.post('/api/auth/signout');
+		if (status !== 200) throw new Error(statusText);
+		location.reload();
+	};
+</script>
+
+<button
+	type="button"
+	class="btn variant-filled-primary hover:variant-filled-secondary ease-in-out duration-300"
+	on:click={onClick}
+>
+	sign out
+</button>
