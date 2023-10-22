259191de8573afde0be00d08b402ff57ce4e642f
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-22 15:24:11 +0700

    feat : declare type

---
+++ src/lib/types/auth.d.ts
@@ -0,0 +1,41 @@
+import { User, Session } from '@supabase/supabase-js';
+
+type SignInProps = {
+	email: string;
+	password: string;
+	debug?: boolean;
+};
+type SignIn = (props: SignInProps) => Promise<{ user: User; session: Session }>;
+
+type SignUpProps = {
+	email: string;
+	name: string;
+	password: string;
+	repetePassword: string;
+	debug?: boolean;
+	force?: boolean;
+};
+type SignUp = (props: SignUpProps) => Promise<Tables<'employees'>>;
+
+type SignOutProps = {
+	debug?: boolean;
+};
+type SignOut = (props: SignOutProps) => Promise<string>;
+
+type DeleteUserProps = {
+	id: string;
+	debug?: boolean;
+};
+type DeleteUser = (props: DeleteUserProps) => Promise<Tables<'employees'>>;
+
+type GetUserProps = {
+	id: string | null;
+	debug?: boolean;
+};
+type GetUser = (props: GetUserProps) => Promise<Tables<'employees'>[] | null>;
+
+type UpdateUserProps = {
+	name?: string;
+	debug?: boolean;
+};
+type UpdateUser = (props: UpdateUserProps) => Promise<Tables<'employees'>>;
