9ab8ee56b0c7d1599f246092115d7271b2228ccd
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-28 19:17:34 +0700

    feat: update env variable

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '$lib/env';
 import type { Database } from '$lib/types/schema';
+import { SUPABASE_URL, SUPABASE_ANON_KEY, SUPABASE_SERVICE_ROLE_KEY } from "$env/static/private"
 
-const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseRoleKey = env.SUPABASE_SERVICE_ROLE_KEY;
-
-export const supabase = createClient<Database>(supabaseUrl!, supabaseKey!);
-export const supabaseAdmin = createClient<Database>(supabaseUrl!, supabaseRoleKey!);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabaseAdmin = createClient<Database>(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
