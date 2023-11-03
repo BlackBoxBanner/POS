6414762f22bc7ad6888b1d97308b5031ec0601fc
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-03 14:24:28 +0700

    feat: add `awesome` async await function handler

--- src/routes/api/auth/users/+server.ts
+++ src/routes/api/auth/users/+server.ts
@@ -1,38 +1,32 @@
 import { getUser, deleteUser, updateUser } from '$lib/auth';
 import type { DeleteUserProps, UpdateUserProps } from '$lib/types/auth';
+import { awesome } from '$lib/utils/awesome';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url }) => {
 	const params = url.searchParams;
 	const id = params.get('id');
 
-	try {
-		return Response.json(await getUser({ id }));
-	} catch (error: unknown) {
-		if (error instanceof Error) return Response.json(error.message, { status: 400 });
-		return Response.json(error, { status: 400 });
-	}
+	const { data, error } = await awesome(() => getUser({ id }))
+	if (error) return Response.json(error.message, { status: 400 });
+	return Response.json(data)
 };
 
 export const DELETE: RequestHandler = async ({ request }) => {
 	const body = (await request.json()) as DeleteUserProps;
 	if (!body.id || body.id === undefined) return Response.json('No id provided.', { status: 400 });
-	try {
-		return Response.json(await deleteUser(body));
-	} catch (error: unknown) {
-		if (error instanceof Error) return Response.json(error.message, { status: 400 });
-		return Response.json(error, { status: 400 });
-	}
+
+	const { data, error } = await awesome(() => deleteUser(body))
+	if (error) return Response.json(error.message, { status: 400 });
+	return Response.json(data)
 };
 
 export const PATCH: RequestHandler = async ({ request }) => {
 	const body = (await request.json()) as UpdateUserProps;
 	if (!body.name || body.name === undefined)
 		return new Response('No name provided.', { status: 400 });
-	try {
-		return Response.json(await updateUser(body));
-	} catch (error: unknown) {
-		if (error instanceof Error) return Response.json(error.message, { status: 400 });
-		return Response.json(error, { status: 400 });
-	}
+
+	const { data, error } = await awesome(() => updateUser(body))
+	if (error) return Response.json(error.message, { status: 400 });
+	return Response.json(data)
 };
