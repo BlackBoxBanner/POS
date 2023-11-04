3acfd14becab780117e4aebc88778f523742e0aa
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-05 01:17:37 +0700

    feat: improve getOrder

--- src/lib/handler/order/index.ts
+++ src/lib/handler/order/index.ts
@@ -7,21 +7,38 @@ type Orders<T, V = Tables<'orders'>> = (props: T) => Promise<V>;
 
 export type GetOrdersProps = {
   id?: string
+  table_id?: string
 }
-export const getOrders: Orders<GetOrdersProps, Tables<"orders">[]> = async ({ id }) => {
+export const getOrders: Orders<GetOrdersProps, Tables<"orders">[]> = async ({ id, table_id }) => {
+  let query = supabase.from("orders").select("*");
+
   if (id) {
-    const { data, error } = await supabase.from("orders").select().eq("id", id)
-    if (error) throw customError({
-      id: "id",
-      message: error.message
-    })
-    return data
-  } else {
-    const { data, error } = await supabase.from("orders").select("*")
-    if (error) throw customError({
-      id: "id",
-      message: error.message
-    })
-    return data
+    query = query.eq("id", id);
+  } else if (table_id) {
+    query = query.eq("id", table_id);
   }
-}
+
+  const { data, error } = await query;
+
+  if (error) {
+    if (id) {
+      throw customError({
+        id: "id",
+        message: `Error fetching order by id: ${error.message}`,
+      });
+    } else if (table_id) {
+      throw customError({
+        id: "table_id",
+        message: `Error fetching order by table_id: ${error.message}`,
+      });
+    } else {
+      throw customError({
+        id: "general",
+        message: `Error fetching orders: ${error.message}`,
+      });
+    }
+  }
+
+  return data;
+};
+
