cea292319f46632a32f489d4a8eeea8b1fea43a7
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-04 14:26:44 +0700

    feat: create `createOrder` function

---
+++ src/lib/handler/order/createOrders.ts
@@ -0,0 +1,16 @@
+import { supabase } from "$lib/supabase"
+import type { Inserts, Tables } from "$lib/types/schema"
+import { customError } from "$lib/utils/errorHandler"
+
+type Orders<T> = (props: T) => Promise<Tables<"orders">>
+
+export type CreateOrderProps = Pick<Inserts<"orders">, "table_id" | "menu_id">
+
+export const createOrder: Orders<CreateOrderProps> = async (props) => {
+  const { data, error } = await supabase.from("orders").insert(props).select()
+  if (error) throw customError({
+    id: "Create Order",
+    message: error.message
+  })
+  return data[0]
+}
