eaf32a7148d2747f5e7642e4a317f39200f1eaa8
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-11-05 23:04:24 +0700

    add a QR billed template

--- src/lib/bill/Enter.svelte
+++ src/lib/bill/Enter.svelte
@@ -1,4 +1,27 @@
 <script lang="ts">
+    export let branch = "KMUTT"
+    export let tableNumber = 1
+    export let customerNumber = 10
+    export let qrURL = "https://www.imgonline.com.ua/examples/qr-code-url.png"
 </script>
 
-<div class="w-[58mm] bg-white text-black flex flex-col h-screen items-center">bill detail</div>
+<div class="w-[58mm] bg-white text-black flex flex-col items-center p-2 relative gap-2">
+	<div class="text-center">
+		<span class="text-xl font-bold">グリル ( GU-RI-RU )</span>
+		<p class="store-detail">สาขา:{branch} <br />โต๊ะหมายเลข {tableNumber} <br /> จำนวนลูกค้า:{customerNumber}คน</p>
+	</div>
+	<div class="border-b-2 border-black w-full" />
+	<div class="image">
+		<img
+			class="w-full h-full bg-cover"
+			src={qrURL}
+			alt="QR for Ordering food"
+		/>
+	</div>
+	<div class="border-b-2 border-black w-full" />
+	<div class="flex flex-col gap-2 items-center text-center">
+		<span>*** คำแนะนำการสั่งอาหาร ***</span>
+		<span>Andriod สามารถสแกนผ่าน LINE หรือ แอปพลิเคชั่นอื่น</span>
+		<span>IOS สามารถสแกนผ่านกล้อง (camera) ได้เลย</span>
+	</div>
+</div>
