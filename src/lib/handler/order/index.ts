745396ca27714b030c86096cde0cace4be4bf7f5
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-05 22:23:15 +0700

    feat: create update function

--- src/lib/handler/order/index.ts
+++ src/lib/handler/order/index.ts
@@ -1,3 +1,21 @@
-import type { Tables } from "$lib/types/schema";
+import { supabase } from "$lib/supabase";
+import type { Tables, Updates } from "$lib/types/schema";
+import { customError } from "$lib/utils/errorHandler";
 
 type Orders<T, V = Tables<'orders'>> = (props: T) => Promise<V>;
+
+export type UpdateOrder = Pick<Updates<"orders">, "menu" | "status" | "id">
+
+export const updateOrder: Orders<UpdateOrder> = async (props) => {
+  if (!props.id) throw customError({
+    id: "id",
+    message: "ID missing"
+  })
+  const { data, error } = await supabase.from("orders").update(props).eq("id", props.id).select()
+
+  if (error) throw customError({
+    id: "id",
+    message: error.message
+  })
+  return data[0]
+}
