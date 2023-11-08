d6b86a2dfbe21d820dae553c539484998dd4d8e8
Sawatsakron <pithwithsomeonelol@gmail.com>
2023-11-08 22:35:46 +0700

    Create dishtype and POST api

--- src/lib/handler/type/index.ts
+++ src/lib/handler/type/index.ts
@@ -1,7 +1,27 @@
-import type { Tables } from '$lib/types/schema';
+import type { Inserts ,Tables } from '$lib/types/schema';
+import { customError } from '$lib/utils/errorHandler';
+import { supabase } from '$lib/supabase';
+
+
 
 type Type<T, V = Tables<'dish_types'>> = (props: T) => Promise<V>;
 
-export const createDishType: Type<{}> = async () => {
-	return;
+export type CreateTypeProps = Pick<Inserts<'dish_types'>, 'name'>
+
+export const createDishType: Type<CreateTypeProps> = async (props) => {
+	
+	if (!props.name) 
+		throw customError({
+			id: 'dish_types',
+			message: 'Please provide a name'	
+		})
+	
+	const { data, error } = await supabase.from('dish_types').insert(props).select();
+	if (error)
+		throw customError({
+			id: 'Create DishType',
+			message: error.message
+		});
+	if (data.length == 0) throw customError({ id: 'id', message: 'No matched ID' });
+	return data[0];
 };
