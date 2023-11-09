e60a287fb35f070accd80b5748f1301c884c8d3b
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-09 15:00:54 +0700

    feat: improve code

---
+++ src/lib/components/bill/MenuItems.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let productName: unknown;
+	export let productAmount: unknown;
+	export let productPrice: unknown;
+</script>
+
+<span class=" text-xs text-left">{productName}</span>
+<span class="text-xs text-right">{productAmount}</span>
+<span class=" text-xs text-right">{productPrice}</span>
