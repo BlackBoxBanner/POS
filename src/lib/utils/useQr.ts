2ee8ebfee48ea710a0a22f7eacdc318c70925cc4
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-09 14:45:35 +0700

    feat: improve enterBill

---
+++ src/lib/utils/useQr.ts
@@ -0,0 +1,10 @@
+import QRCode from 'qrcode';
+
+type UseQr = (props: UseQrProps) => Promise<string>
+export type UseQrProps = {
+  url: string,
+  type?: "string"
+}
+export const useQr: UseQr = async ({ url, type }) => {
+  return type == "string" ? QRCode.toString(url) : QRCode.toDataURL(url)
+}
