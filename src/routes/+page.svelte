1f833e6e1591d2f7a55fbb9e09421a4936bba63a
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-15 20:53:23 +0700

    feat: style components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Signout from '$lib/components/signout.svelte';
+	import Logo from '$lib/components/logo.svelte';
+	import Icon from '@iconify/svelte';
+	import eyeIcon from '@iconify/icons-ion/eye';
+	import eyeOffOutline from '@iconify/icons-ion/eye-off-outline';
+
 	export let data: PageData;
 </script>
 
 <div class="font-exo h-full flex justify-center items-center flex-col gap-4">
-	<h1 class="text-4xl font-bold">
-		{`グリル ( GU-RI-RU )`}
-	</h1>
+	<Logo />
 	<div>
 		{#if !data.session}
 			<a class="" href="/auth/signin">sign in</a>
@@ -16,4 +19,6 @@
 			<Signout />
 		{/if}
 	</div>
+	<Icon icon={eyeIcon} />
+	<Icon icon={eyeOffOutline} />
 </div>
