477fc650e612c433227c9c8cbfbfc0e58be14129
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-18 21:08:24 +0700

    feat: create Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { cn } from '@dookdiks/utils';
+	import type { Action } from 'svelte/action';
+
+	export let ref: HTMLButtonElement | null = null;
+
+	type ButtonSize = 'small' | 'medium' | 'large';
+	export let size: ButtonSize = 'medium';
+
+	$: btnSize =
+		size === 'small'
+			? 'text-xs py-2 px-4'
+			: size === 'medium'
+			? 'text-base py-2 px-6 min-w-[10rem]'
+			: size === 'large'
+			? 'text-xl py-2 px-6 min-w-[10rem]'
+			: '';
+</script>
+
+<button
+	{...$$restProps}
+	bind:this={ref}
+	on:click
+	on:focus
+	on:mouseover
+	on:mouseenter
+	on:mouseleave
+	class={cn(
+		'bg-crayola-base text-milk-base rounded-full',
+		'ease-in-out duration-300',
+		'hover:bg-crayola-200',
+		btnSize,
+		$$restProps.class
+	)}
+>
+	<slot />
+</button>
