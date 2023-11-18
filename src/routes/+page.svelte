99515a6435da9058efbef98767eeb050cc53fad7
NongEs <133220527+NongEs@users.noreply.github.com>
2023-11-19 01:44:33 +0700

    create login page and implement login function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
 	import Logo from '$lib/components/logo.svelte';
 	import type { PageData } from './$types';
-
+	import Email from '$lib/components/input/Email.svelte';
+	import Password from '$lib/components/input/Password.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import { axiosInstant } from '$lib/axios';
+	import { awesome } from '@dookdiks/utils';
+	async function Login() {
+		
+		const { data, error } = await awesome.async( () =>
+			axiosInstant.post('http://localhost:5173/api/auth/signin', {
+				email,
+				password 
+				
+			})
+		);
+		console.log({data})
+	}
 	export let data: PageData;
+
+	let email="";
+	let password="";
 </script>
 
-<div class="font-exo h-full flex justify-center items-center flex-col gap-4">
+<div class="bg-ivory-base font-exo h-full flex justify-center items-center flex-col gap-4">
 	<Logo />
+	<form class="w-1/2 flex justify-center items-center flex-col">
+		<Email bind:value={email} inputClass="flex w-full item-left p-4"/> 
+		<Password bind:value={password} />
+		<Button on:click={Login} class="flex justify-center items-center rounded-full text-xl w-1/3 mt-4">Login</Button>
+	</form>
 </div>
