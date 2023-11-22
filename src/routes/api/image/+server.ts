24983447e104d1a4142dfa177587395c1d8d5f1b
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-22 14:03:59 +0700

    feat: create upload image

--- src/routes/api/image/+server.ts
+++ src/routes/api/image/+server.ts
@@ -1,15 +1,17 @@
-import { awesome } from '@dookdiks/utils';
 import type { RequestHandler } from './$types';
-import { postImageUrl } from '$lib/handler/image';
-
-const exampleFunc = async () => {
-  return 'example function toggle';
-};
+import { getImageUrl, postImageUrl } from '$lib/handler/image';
+import { toFile } from '$lib/utils/image';
 
 export const POST: RequestHandler = async ({ request }) => {
-  const { image } = (await request.json()) as { image: string }
+  const { image, name } = (await request.json()) as { image: string, name: string }
+  const file = await toFile(image, name)
 
-  const { data, error } = await awesome.async(() => postImageUrl("name", image));
-  if (error) return Response.json(error.message, { status: 400 });
-  return Response.json(data);
+  const { data: path, error: pathError } = await postImageUrl(file.name, file)
+
+  if (pathError) return Response.json(pathError.message, { status: 400 });
+
+  const { data: imageUrl, error: imageError } = await getImageUrl(path)
+  if (imageError) return Response.json(imageError.message, { status: 400 });
+
+  return Response.json(imageUrl);
 };
