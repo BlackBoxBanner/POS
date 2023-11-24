2c086d47e8794379e9cddc1d4d65a2d994c42b87
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-24 22:02:21 +0700

    feat: format the code

--- src/lib/handler/image/index.ts
+++ src/lib/handler/image/index.ts
@@ -3,34 +3,34 @@ import { customError } from '@dookdiks/error';
 import { awesome } from '@dookdiks/utils';
 
 const getFoodBucket = () => {
-  return supabaseAdmin.storage.from("food")
-}
+	return supabaseAdmin.storage.from('food');
+};
 
-export type GetImageUrlHandle = (path: string) => Promise<string>
+export type GetImageUrlHandle = (path: string) => Promise<string>;
 
 export const getImageUrlHandler: GetImageUrlHandle = async (path) => {
-  const bucket = getFoodBucket()
-  const imageUrl = bucket.getPublicUrl(path)
-  const responseDate = await fetch(imageUrl.data.publicUrl)
+	const bucket = getFoodBucket();
+	const imageUrl = bucket.getPublicUrl(path);
+	const responseDate = await fetch(imageUrl.data.publicUrl);
 
-  if (!responseDate.ok) throw customError({ id: "image", message: "No image found" })
-  return responseDate.url
-}
+	if (!responseDate.ok) throw customError({ id: 'image', message: 'No image found' });
+	return responseDate.url;
+};
 
-export type UploadImageHandler = (name: string, file: File) => Promise<string>
+export type UploadImageHandler = (name: string, file: File) => Promise<string>;
 
 export const uploadImageHandler = async (name: string, file: File) => {
-  const bucket = getFoodBucket()
-  const { data, error } = await bucket.upload(name, file)
-  
-  if (error) throw customError({ id: "image", message: error.message })
-  return data.path
-}
+	const bucket = getFoodBucket();
+	const { data, error } = await bucket.upload(name, file);
+
+	if (error) throw customError({ id: 'image', message: error.message });
+	return data.path;
+};
 
 export const getImageUrl = async (path: string) => {
-  return awesome.async(() => getImageUrlHandler(path))
-}
+	return awesome.async(() => getImageUrlHandler(path));
+};
 
 export const postImageUrl = async (name: string, file: File) => {
-  return awesome.async(() => uploadImageHandler(name, file))
-}
+	return awesome.async(() => uploadImageHandler(name, file));
+};
--- src/routes/api/image/+server.ts
+++ src/routes/api/image/+server.ts
@@ -3,15 +3,15 @@ import { getImageUrl, postImageUrl } from '$lib/handler/image';
 import { toFile } from '$lib/utils/image';
 
 export const POST: RequestHandler = async ({ request }) => {
-  const { image, name } = (await request.json()) as { image: string, name: string }
-  const file = await toFile(image, name)
+	const { image, name } = (await request.json()) as { image: string; name: string };
+	const file = await toFile(image, name);
 
-  const { data: path, error: pathError } = await postImageUrl(file.name, file)
+	const { data: path, error: pathError } = await postImageUrl(file.name, file);
 
-  if (pathError) return Response.json(pathError.message, { status: 400 });
+	if (pathError) return Response.json(pathError.message, { status: 400 });
 
-  const { data: imageUrl, error: imageError } = await getImageUrl(path)
-  if (imageError) return Response.json(imageError.message, { status: 400 });
+	const { data: imageUrl, error: imageError } = await getImageUrl(path);
+	if (imageError) return Response.json(imageError.message, { status: 400 });
 
-  return Response.json(imageUrl);
+	return Response.json(imageUrl);
 };
