9bc3b7ca2a10b6ef731b313bc3d977f9946e521e
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-12-05 19:59:30 +0700

    :construction: add component of add list

---
+++ src/lib/components/navbar/add menu/AddMenuSideNavBtn.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { cn } from '@dookdiks/utils';
+	export let href: string;
+	export let label: string;
+	export let active: boolean = false;
+</script>
+
+<button
+	class={cn(
+		'text-philippine-gray-base bg-ivory-400 w-full p-8 flex border-b border-b-timberwolf-base',
+		active && 'bg-blue-sapphire-base text-ivory-400'
+	)}
+	on:click={async () => await goto(`/dashboard/menu/add/${href}`)}
+>
+	{label.toUpperCase()}
+</button>
